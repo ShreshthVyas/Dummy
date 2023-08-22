896ba5ebc69f93b836dcdd9cf5f959d5f910d92f
ShreshthVyas <shreshth.vyas.2000@gmail.com>
2023-08-22 21:07:06 +0530

    Second commit

---
+++ src/DummyPackage/DummyClass.java
@@ -0,0 +1,11 @@
+package DummyPackage;
+
+public class DummyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi this is a Dummy Class");
+
+	}
+
+}
